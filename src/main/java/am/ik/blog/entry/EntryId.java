3e004060598ced547832975f9ff625ada73e6066
Toshiaki Maki <tmaki@pivotal.io>
2018-01-12 02:54:30 +0900

    Add fromFilePath

--- src/main/java/am/ik/blog/entry/EntryId.java
+++ src/main/java/am/ik/blog/entry/EntryId.java
@@ -1,9 +1,9 @@
 package am.ik.blog.entry;
 
 import java.io.Serializable;
+import java.nio.file.Path;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -27,5 +27,9 @@ public class EntryId implements Serializable {
 	public static EntryId fromFileName(String fileName) {
 		String entryId = fileName.replace(".md", "");
 		return new EntryId(Long.valueOf(entryId));
+	}
+
+	public static EntryId fromFilePath(Path filePath) {
+		return fromFileName(filePath.getFileName().toString());
 	}
 }
