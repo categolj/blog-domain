7fb6c0d0d6250aea8d757dfb88cc691238d192dc
Toshiaki Maki <tmaki@pivotal.io>
2018-08-11 13:06:28 +0900

    Nullcheck

--- src/main/java/am/ik/blog/entry/json/FrontMatterDeserializer.java
+++ src/main/java/am/ik/blog/entry/json/FrontMatterDeserializer.java
@@ -1,18 +1,17 @@
 package am.ik.blog.entry.json;
 
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.StreamSupport.stream;
-
 import java.io.IOException;
 
-import org.springframework.boot.jackson.JsonObjectDeserializer;
-
+import am.ik.blog.entry.*;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 
-import am.ik.blog.entry.*;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
 
 public class FrontMatterDeserializer extends JsonObjectDeserializer<FrontMatter> {
 
@@ -21,10 +20,13 @@ public class FrontMatterDeserializer extends JsonObjectDeserializer<FrontMatter>
 			DeserializationContext deserializationContext, ObjectCodec objectCodec,
 			JsonNode jsonNode) throws IOException {
 		Title title = new Title(nullSafeValue(jsonNode.get("title"), String.class));
-		Tags tags = new Tags(stream(jsonNode.get("tags").spliterator(), false)
-				.map(n -> new Tag(n.asText())).collect(toList()));
-		Categories categories = new Categories(
-				stream(jsonNode.get("categories").spliterator(), false)
+		JsonNode tagsNode = jsonNode.get("tags");
+		Tags tags = tagsNode == null ? new Tags()
+				: new Tags(stream(tagsNode.spliterator(), false)
+						.map(n -> new Tag(n.asText())).collect(toList()));
+		JsonNode categoriesNode = jsonNode.get("categories");
+		Categories categories = categoriesNode == null ? new Categories()
+				: new Categories(stream(categoriesNode.spliterator(), false)
 						.map(n -> new Category(n.asText())).collect(toList()));
 		PremiumPoint point = new PremiumPoint(
 				nullSafeValue(jsonNode.get("point"), Integer.class));
